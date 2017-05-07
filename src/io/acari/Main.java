9b09ec1ca33837f89897e12c9b06ba8fe8330a8b
cyclic-reference <alex@cyclicreference.space>
2017-05-07 10:47:08 -0500

    Read Write, Generify

--- src/io/acari/Main.java
+++ src/io/acari/Main.java
@@ -1,32 +1,58 @@
 package io.acari;
 
+import io.acari.pojo.ExternalizableProgrammer;
+import io.acari.pojo.Programmer;
 import io.acari.repositories.ProgrammerRepository;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Path fileToWrite = Paths.get("programmers.data");
-        if(Files.notExists(fileToWrite)){
+        ProgrammerRepository programmerRepository = ProgrammerRepository.newProgrammerRepository();
+        readWriteObject(programmerRepository.getProgrammers(), Programmer.class);
+        readWriteObject(programmerRepository.getProgrammers().map(ExternalizableProgrammer::new), ExternalizableProgrammer.class);
+    }
+
+    private static <T> void readWriteObject(Stream<T> objectStream, Class<T> tClass) throws IOException {
+        String simpleName = tClass.getSimpleName();
+        Path fileToWrite = Paths.get(simpleName + ".data");
+        if (Files.notExists(fileToWrite)) {
             Files.createFile(fileToWrite);
         }
         try (ObjectOutputStream out = new ObjectOutputStream(
                 Files.newOutputStream(fileToWrite, StandardOpenOption.TRUNCATE_EXISTING))) {
-            ProgrammerRepository programmerRepository = ProgrammerRepository.newProgrammerRepository();
-            programmerRepository.getProgrammers().forEach(programmer -> {
+            objectStream.forEach(object -> {
                 try {
-                    out.writeObject(programmer);
+                    out.writeObject(object);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
         }
 
+
+        try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(fileToWrite, StandardOpenOption.READ))) {
+            List<T> programmers = new LinkedList<>();
+            try {
+                while (true) {
+                    programmers.add((T) in.readObject());
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (EOFException ignored) {
+            }
+            System.out.format("%d %s read from file!\n", programmers.size(), simpleName);
+        }
     }
 }
